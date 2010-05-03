31ba35e46fc06cf6306ba694b9c62fbdbb391f5c
reza <reza@3f84299c-254d-11df-a3f3-6f764f7735a5>
2010-05-03 20:40:36 +0000

    cylinder.h added
    git-svn-id: svn+ssh://comphys200.ethz.ch/home/reza/svn/MD/trunk@47 3f84299c-254d-11df-a3f3-6f764f7735a5

---
+++ cylinder.h
@@ -0,0 +1,35 @@
+#ifndef CYLINDER_H
+#define CYLINDER_H 
+
+#include"geombase.h"
+
+using namespace math;
+
+template<>
+class GeomObject<tcylinder>: public GeomObjectBase{
+	public:	
+	GeomObject(const vec &v1,const vec &v2, double _r):GeomObjectBase((v1+v2)/2.0,tcylinder), X1(v1), X2(v2), r(_r){
+		identifier=5;
+		}
+
+	void shift(const vec&){};
+	void rotate(const vec& n, double alpha){ };
+	void rotateTo(const Quaternion &q){};
+	double vol(){};
+	double I(vec n){};
+
+	void scale(double){};
+	void print(std::ostream &out)const{
+		out<< identifier<< "   ";
+		out<< X1<< "  "<<r<<"  ";
+		out<< X2<< "  "<<r;
+		};
+	void parse(std::istream &in){};
+
+	vec X1, X2;
+	double r;
+
+	};
+
+typedef GeomObject<tcylinder> CCylinder;
+#endif
