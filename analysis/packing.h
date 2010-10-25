926af7bfc48509a1144d4e355ae0556937e2da23
Reza Mahmoodi Baram <reza@reza-cftc.(none)>
2010-10-25 19:13:15 +0100

    little changes

--- analysis/packing.h
+++ analysis/packing.h
@@ -5,6 +5,7 @@
 #include"../ellips_contact.h"
 #include"../MersenneTwister.h"
 #include"../interaction.h"
+#include"contact_network.h"
 
 
 
@@ -13,8 +14,6 @@ vec randomVec(const vec &x1, const vec &x2){
 	}
 
 //member classes for contact network
-template<class T>
-class ContactNetwork;
 
 template < class T>
 class CPacking : public vector <T *>
@@ -163,71 +162,4 @@ void print_connectivity(vector<CCircle> &packing, const char * name){
 
 */
 
-template<class T>
-class ContactNetwork 
-	{
-	class Contact : public BasicContact{ 
-		public: 
-		Contact(T *_p1, T *_p2, vec _x, vec _n):BasicContact(_x, _n), p1(_p1), p2(_p2){} ; 
-		Contact(T *_p1, T *_p2, const BasicContact &bc):BasicContact(bc), p1(_p1), p2(_p2){} ; 
-		T * p1, *p2; 
-		}; 
-	public:
-	ContactNetwork(CPacking<T> *_p):N(0), elems(NULL), packing(_p){}
-	~ContactNetwork(){
-		delete [] elems;
-		}
-
-	void build(){
-		typename CPacking<T>::const_iterator it;
-		N=packing->size();
-		elems= new vector<Contact>[N];
-		assert(elems);
-		for(size_t i=0;  i<N; i++){
-			for(size_t j=i+1;  j<N; j++){
-				ShapeContact ovs(packing->at(i),packing->at(i));
-				//ovs.back();
-				CInteraction::overlaps(&ovs, packing->at(i), packing->at(j) ); 
-				if(!ovs.empty()) {
-					assert(ovs.size()==1);
-					elems[i].push_back(Contact(packing->at(i), packing->at(j), static_cast<BasicContact>(ovs.back()) ));
-					elems[j].push_back(Contact(packing->at(j), packing->at(i), static_cast<BasicContact>(ovs.back()) ));
-					}
-				}
-			}
-
-		/*
-		for(size_t i=0;  i<NContactNetwork; i++){
-		for(size_t j=0; j<elems[i].size(); j++){
-			elems[i].at(j).l
-			}
-		*/
-	}
-
-	void print(ostream &out)const{
-		assert(elems);
-		assert(N);
-		cerr<< N <<endl;
-		for(size_t i=0; i<N; i++){
-			out<<"2  ";
-			out<<packing->at(i)->Xc<<"  0.008  0 0 50000"<<endl;;
-		for(size_t j=0; j<elems[i].size(); j++){
-			//if(elems[i].at(j).p1->Xc(1)>0.12)continue;
-			//out<<"2  ";
-			//out<<elems[i].at(j).x<<"  0.005  0 0 50000"<<endl;;
-
-			out<<"5  ";
-			out<<elems[i].at(j).p1->Xc<<"  0.001  ";
-			out<<elems[i].at(j).x<<"  0.005  50000 50000 50000"<<endl;;
-
-			}
-			}
-	}
-		
-
-	size_t N;
-	private:
-	vector<Contact> *elems;
-	CPacking<T> *packing;
-	};
 #endif /* PACKING_H */
