472df5ab340b5e1dec31ed1b2bb11e0d9565acb1
Reza Mahmoodi Baram <reza@reza-cftc.(none)>
2010-10-28 12:40:51 +0100

    little changes. remove a big in destructor of packing.h

--- genFrames.sh
+++ genFrames.sh
@@ -3,17 +3,20 @@ files=$*
 w=500
 h=500
 
+rasterconverter=$HOME/workstation/MD/analysis/convert2raster
 echo "Generating jpg frames "
 for file in $files
 do
 	#/Users/reza/bin/test.perl -x0 -y0 -s1  -rx75  $file | render -size $w"x"$h -jpeg > $file.jpg 
-	if [ -f _$file.jpg ]
+	filename=`basename $file`
+	filedir=`dirname $file`
+	if [ -f _$filename.jpg ]
 	then
 		continue
 	fi
-	/Users/reza/workstation/MD/analysis/convert2raster $file > $file".r"
-	coord2pr3d -x0 -y0 -z0  -s1 $file".r" | render -size $w"x"$h -jpeg > _$file.jpg 
-	rm -f $file".r"
+	$rasterconverter $file > $filename".r"
+	coord2pr3d -x0 -y0 -z0  -s1 $filename".r" | render -size $w"x"$h -jpeg > _$filename.jpg 
+	rm -f $filename".r"
 	#coord2pr3d -x0 -y0 -s1 $file | render -size $w"x"$h -jpeg > _$file.jpg 
 	
 
